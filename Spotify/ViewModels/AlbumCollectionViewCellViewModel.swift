26a8063e19f23c7ada45440cc1fba3a458fcb88f
arthris <oclockth@gmail.com>
2021-07-15 14:55:02 +0700

    Complete Home Tab & Start working on Search Tab

---
+++ Spotify/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Paratthakorn Sribunyong on 15/7/2564 BE.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
